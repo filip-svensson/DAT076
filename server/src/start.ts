5ebee12a93ad37e0aa8e57c6ae5a21049d0d5955
filip-svensson <fils@student.chalmers.se>
2023-02-15 11:04:08 +0100

    Co-authored-by: Raggan2000 <ernstn@chalmers.se> Co-authored-by: Skinkan3000 <casperv@chalmers.se> Co-authored-by: PieFreak <lukas.wigren@outlook.com>

--- server/src/start.ts
+++ server/src/start.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import { postRouter } from "./router/Post";
+import cors from "cors";
 
 export const app = express();
 
-app.use(express.json());
+app.use(cors());
 app.use("/Post", postRouter);
